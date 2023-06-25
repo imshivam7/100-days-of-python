66e9442684533cb0d47b7957ba53f0b7cc3d4d7e
imshivam7 <136987514+imshivam7@users.noreply.github.com>
2023-06-25 23:44:03 +0530

    cypher code push

---
+++ Function_d8.py/function.py
@@ -0,0 +1,19 @@
+# def greet():
+#     print("Hello")
+#     print("world")
+#     print("Morning")
+    
+# greet() 
+
+
+
+# def greet_with(name, location):
+#     print(f"hello {name}")
+#     print(f"What is the weather in {location}")
+# greet_with("shivam", "varanasi")
+    
+    
+def greet_with(name="shivam", location="varanasi"):
+    print(f"hello {name}")
+    print(f"What is the weather in {location}")
+greet_with()
--- Tip-calculator.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the tip calculatror.")
-bill=float(input("What was the total bill?" ))
-tip=int(input("what percentage of tip would you like to give? 10, 12, or 15?"))
-people=int(input("How many people to split the bill"))
-total_bill = tip / 100 * bill + bill
-bill_per_person=(total_bill / people)
-final_amount=round(bill_per_person, 2)
-print(f"Each person should pay {final_amount}")
